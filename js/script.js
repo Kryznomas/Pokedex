30a7e4f6f60361187bc7a0c7e94523e548de411a
kryznomas <kryztop@gmail.com>
2021-01-22 00:22:51 -0300

    script con todo funcionando

--- js/script.js
+++ js/script.js
@@ -1,29 +1,44 @@
 $(document).ready(function () {
-var inputNombre = $('#input-nombre').value;
-    let pokemon = $.ajax({
-    method: "GET",
-    url: `https://pokeapi.co/api/v2/pokemon/bulbasaur/${inputNombre}`,
-    success: function(data) {
-    
-    return data;
-    },
-    dataType: "json",
+
+    let buttonNombre = $('#button-nombre');
+    let buttonRandom = $('#button-random');
+
+    $(buttonNombre).click(function () {
+        var inputNombre = $('#input-nombre').val();
+        $.ajax({
+            method: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${inputNombre}`,
+            success: function (data) {
+                llenarCampos(data);
+                return data;
+            },
+            dataType: "json",
+        });        
     });
-    
-    let buttonNombre = $('#button-nombre')
-    
-    let imgPokemon = $('#img-pokemon');
-    let nombrePokemon = $('#nombre-pokemon');
-   
-    $(buttonNombre).click(function(e){
-        e.prevenDefault;
-        nombrePokemon.text(pokemon.responseJSON["name"]);
-        imgPokemon.attr("src",pokemon.responseJSON["sprites"]["other"]["official-artwork"]["front_default"]);
-});
-        console.log(pokemon);
 
-    })
+    $(buttonRandom).click(function () {
+        var randomNumber = Math.floor(Math.random()* (+1 - +150) + +150);  
+        $.ajax({
+            method: "GET",
+            url: `https://pokeapi.co/api/v2/pokemon/${randomNumber}`,
+            success: function (data) {
+                llenarCampos(data);
+                return data;
+            },
+            dataType: "json",
+        });        
+    });
 
+    function llenarCampos(pokemon) {
+
+        let imgPokemon = $('#img-pokemon');
+        let nombrePokemon = $('#nombre-pokemon');
+
+        nombrePokemon.text(pokemon["name"]);
+        imgPokemon.attr("src", pokemon["sprites"]["other"]["official-artwork"]["front_default"]);
+    }
+
+})
 
 
 
